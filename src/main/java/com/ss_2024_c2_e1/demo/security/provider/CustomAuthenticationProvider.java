438d173fcaf6443158ed58168737934c5754e461
craxex9141 <uzairshaikh0702@gmail.com>
2024-01-06 13:05:31 +0530

    #2.1  minor change

--- src/main/java/com/ss_2024_c2_e1/demo/security/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/ss_2024_c2_e1/demo/security/provider/CustomAuthenticationProvider.java
@@ -27,6 +27,7 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public boolean supports(Class<?> authentication) {
+
         return CustomAuthentication.class.equals(authentication);
     }
 }

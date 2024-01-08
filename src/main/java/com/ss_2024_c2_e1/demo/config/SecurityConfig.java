818f53a354564ff55f7d33856e9b8a2e3c1fb28e
craxex9141 <uzairshaikh0702@gmail.com>
2024-01-08 12:36:02 +0530

    #3.1 removing depricated methods

--- src/main/java/com/ss_2024_c2_e1/demo/config/SecurityConfig.java
+++ src/main/java/com/ss_2024_c2_e1/demo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.ss_2024_c2_e1.demo.config.filter.ApiKeyFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
@@ -15,12 +16,11 @@ public class SecurityConfig {
     private String key;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http_security) throws Exception {
-        return  http_security.httpBasic()
-                .and()
-                .addFilterBefore(new ApiKeyFilter(key), BasicAuthenticationFilter.class)
-                .authorizeRequests().anyRequest().authenticated()
+          http_security.authorizeHttpRequests((authorizeRequests)->authorizeRequests.anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
+                .addFilterBefore(new ApiKeyFilter(key), BasicAuthenticationFilter.class);
+
                 //.and().authenticationManager()  //or by adding a bean of type AuthenticationMAnager
                 //.and().authenticationProvider() //it doesn't ovverride  the AuthenticationProvider , it adds one more to the collection
-                .and().build();
+        return http_security.build();
     }
 }
